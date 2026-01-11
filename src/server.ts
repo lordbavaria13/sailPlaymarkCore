6a41f3e5d2d2f27e4ec947719e40fb380ab338a8
LordBavaria13 <harant14@googlemail.com>
2026-01-11 23:37:51 +0100

    render fix for deployment adress

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ const init = async () => {
 
     const server = Hapi.server({
         port: process.env.PORT || 10000,
-        host: "0.0.0.0"
+        host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost"
     });
 
     await server.register(Vision);
